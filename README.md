5ec3135da5c3803377cdc2d42f4d665a1e0b6def
Dani <dan.avila7@gmail.com>
2023-02-08 09:33:56 -0500

    readme, gitignore, readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# YoutubeGPT
+<h1 align="center">
+YoutubeGPT 🤖
+</h1>
+With Youtube GPT you will be able to extract all the information from a video on YouTube just by pasting the video link.
+You will obtain the transcription, the embedding of each segment and also ask questions to the video through a chat.
+
+All code was written with the help of <a href="https://codegpt.co">Code GPT</a>
+<hr>
+<br>
+
+# 🔧 Features
+
+- Video transcription with **OpenAI Whisper**
+- Embedding Transcript Segments with the OpenAI API (**text-embedding-ada-002**)
+- Chat with the video using **streamlit-chat** and OpenAI API (**text-davinci-003**)
+
+# Dependencies
+
+These dependencies are required to install with the requirements.txt file:
+
+* streamlit 
+* streamlit_chat 
+* matplotlib 
+* plotly 
+* scipy 
+* sklearn 
+* pandas 
+* numpy 
+* git+https://github.com/openai/whisper.git 
+* pytube 
+* openai-whisper
+
+To install the dependencies with this file, you will need to use the pip command. The pip command will read the requirements.txt file and download and install all the dependencies listed in it. The command is the following:
+
+```bash
+pip install -r requirements.txt
+```
--- app.py
+++ app.py
@@ -5,12 +5,14 @@ import whisper
 from pytube import YouTube
 from streamlit_chat import message
 import openai
-from openai.embeddings_utils import get_embedding, cosine_similarity
-#openai.api_key = st.secrets["apikey"]
+from openai.embeddings_utils import get_embedding, distances_from_embeddings
+import os
 
 # whisper
 model = whisper.load_model('base')
 output = ''
+data = []
+embeddings = []
 
 # Sidebar
 with st.sidebar:
@@ -19,30 +21,49 @@ with st.sidebar:
                                 type = "password")
     youtube_link = st.text_input(label = ":red[Youtube link]",
                                 placeholder = "")
-    if youtube_link:
+    if youtube_link and user_secret:
         youtube_video = YouTube(youtube_link)
         streams = youtube_video.streams.filter(only_audio=True)
         stream = streams.first()
         if st.button("Start Analysis"):
+            if os.path.exists("word_embeddings.csv"):
+                os.remove("word_embeddings.csv")
+                
             with st.spinner('Running process...'):
                 mp4_video = stream.download(filename='youtube_video.mp4')
                 audio_file = open(mp4_video, 'rb')
                 st.write(youtube_video.title)
-                audio_bytes = audio_file.read()
-                st.audio(audio_bytes, format='audio/ogg')
+                #audio_bytes = audio_file.read()
+                #st.audio(audio_bytes, format='audio/ogg')
+                st.video(youtube_link) 
                 output = model.transcribe("youtube_video.mp4")
+                segments = output['segments']
+                for segment in segments:
+                    openai.api_key = user_secret
+                    response = openai.Embedding.create(
+                        input= segment["text"].strip(),
+                        model="text-embedding-ada-002"
+                    )
+                    embeddings = response['data'][0]['embedding']
+                    meta = {
+                        "text": segment["text"].strip(),
+                        "start": segment['start'],
+                        "end": segment['end'],
+                        "embedding": embeddings
+                    }
+                    data.append(meta)
+                pd.DataFrame(data).to_csv('word_embeddings.csv') 
                 st.success('Analysis completed')
 
 st.title("Youtube GPT 🤖 ")
-# chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
-# with tab2:
-#     st.area_chart(chart_data)
 tab1, tab2 = st.tabs(["Transcription", "Chat with the Video"])
 with tab1: 
     st.header("OpenAI Whisper:")
-    st.write(output)
+    if os.path.exists("word_embeddings.csv"):
+        df = pd.read_csv('word_embeddings.csv')
+        st.write(df)
 with tab2:
-    # Storing the chat
+    st.header("Ask me something about the video:")
     if 'generated' not in st.session_state:
         st.session_state['generated'] = []
 
@@ -55,8 +76,21 @@ with tab2:
 
     user_input = get_text()
 
+    def get_embedding_text(api_key, prompt):
+        openai.api_key = user_secret
+        response = openai.Embedding.create(
+            input= prompt.strip(),
+            model="text-embedding-ada-002"
+        )
+        q_embedding = response['data'][0]['embedding']
+        df=pd.read_csv('word_embeddings.csv', index_col=0)
+        df['embedding'] = df['embedding'].apply(eval).apply(np.array)
+
+        df['distances'] = distances_from_embeddings(q_embedding, df['embedding'].values, distance_metric='cosine')
+        df.sort_values("distances", ascending=False).head(1)
+        return df['text'][0]
+
     def generate_response(api_key, prompt):
-        openai.api_key = api_key
         one_shot_prompt = '''I am YoutubeGPT, a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer.
         Q: What is human life expectancy in the United States?
         A: Human life expectancy in the United States is 78 years.
@@ -74,7 +108,9 @@ with tab2:
         return message
 
     if user_input:
-        output = generate_response(user_secret, user_input)
+        text_embedding = get_embedding_text(user_secret, user_input)
+        user_input_embedding = 'with this context "'+text_embedding+'", answer this question'+user_input
+        output = generate_response(user_secret, user_input_embedding)
         st.session_state.past.append(user_input)
         st.session_state.generated.append(output)
     if st.session_state['generated']:
